e99e1dfc5f582a9579b6e066085d68bde432759b
Abdurahmon <rahmlow.1217@mail.ru>
2025-01-09 07:14:14 +0500

    add 1file

--- main.js
+++ main.js
@@ -4,20 +4,25 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
 for (let i of btnsOpenModal) {
-    i.addEventListener("click", function () {
-        modal.classList.remove("hidden");
-        overlay.classList.remove("hidden");
-    });
+    i.addEventListener("click", removeModal);
 }
 
-btnCloseModal.addEventListener("click", function () {
+function removeModal() {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+btnCloseModal.addEventListener("click", addModal);
+
+function addModal() {
     modal.classList.add("hidden");
     overlay.classList.add("hidden");
+}
+overlay.addEventListener("click", addModal);
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        modal.classList.add("hidden");
+        overlay.classList.add("hidden");
+    }
 });
-
-
-overlay.addEventListener("click", function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-})
-
