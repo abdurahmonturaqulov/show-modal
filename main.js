83c6f48c5ca42528f0fe647c78175551df88ed4d
Abdurahmon <rahmlow.1217@mail.ru>
2025-06-06 14:25:13 +0500

    adds5files

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+for (let i of btnsOpenModal) {
+    i.addEventListener("click", removeModal)
+}
+
+function removeModal () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden")
+}
+
+btnCloseModal.addEventListener("click", addModal);
+
+function addModal() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+   }
+
+overlay.addEventListener("click", addModal);
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        modal.classList.add("hidden");
+        overlay.classList.add("hidden");
+    }
+});
